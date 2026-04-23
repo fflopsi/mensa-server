db83e0f45494222cc27c62f37a298bdbfe25b68c
fflopsi <florian.l.frauenfelder@gmail.com>
2026-04-23 13:31:31 +0200

    Static redirect "/" to my website

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,23 +1,11 @@
 package ch.florianfrauenfelder
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import java.sql.Connection
-import java.sql.DriverManager
-import org.jetbrains.exposed.sql.*
+import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        // Static plugin. Try to access `/static/index.html`
-        staticResources("/static", "static")
-    }
+  routing {
+    staticResources("/", "static")
+  }
 }
